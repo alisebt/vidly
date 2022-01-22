2d38d91dd4924f8159c7a032f39233913ccc734b
Ali Sebtonabi <alisebt@outlook.com>
2022-01-22 12:56:43 +0330

    exclude all .log files from repository

--- startup/logging.js
+++ startup/logging.js
@@ -4,7 +4,8 @@ require("express-async-errors");
 
 module.exports = function () {
     winston.handleExceptions(
-        new winston.transports.File({ filename: 'logfile.log' })
+        new winston.transports.Console({ colorize: true, prettyPrint: true }),
+        new winston.transports.File({ filename: 'uncaughtExceptions.log' })
     )
 
     process.on('unhandledRej ection', (ex) => {
